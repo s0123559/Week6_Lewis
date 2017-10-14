1e7a19c512929872c24e908ff83bc0901b46acbe
Jenny <cis290@PC22765-3862.local>
2017-10-14 11:18:01 -0500

    Week6Lewis

---
+++ HideKeyBoard/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  HideKeyBoard
+//
+//  Created by cis290 on 10/14/17.
+//  Copyright © 2017 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func btnView(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtLast: UITextField!
+    @IBOutlet weak var txtFirst: UITextField!
+    @IBOutlet weak var txtContacts: UIButton!
+    
+    @IBAction func btnBack(_ sender: Any) {
+    }
+    @IBOutlet weak var btnBack: UIButton!
+
+
+   
+    @IBAction func btnSave(_ sender: Any) {
+    }
+    }
+    func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    
+    func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+
